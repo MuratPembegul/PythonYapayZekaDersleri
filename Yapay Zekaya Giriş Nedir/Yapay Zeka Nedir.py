496f2c048c74793efcd7752241e87e17707a460a
Murat Pembegül <mpembegul15@gmail.com>
2023-09-01 22:09:24 +0300

    Add files via upload

---
+++ Yapay Zekaya Giriş Nedir/Yapay Zeka Nedir.py
@@ -0,0 +1,14 @@
+""" Yapay zeka, bilgisayar sistemlerinin insana benzer
+zekâsal yetenekleri simüle etme amacıyla geliştirilen
+bir teknoloji ve bilim dalıdır. Temelde, bilgisayar 
+sistemlerinin düşünme, öğrenme, problem çözme, anlama, 
+dil işleme gibi insan benzeri zekâsal yetenekleri 
+sergilemeleri için çalışılır.
+Yapay zeka, bilgisayarları sadece belirli görevleri 
+yürüten araçlar olarak değil, aynı zamanda veriyi 
+analiz edebilen, öğrenebilen ve karmaşık kararlar 
+alabilen akıllı sistemler olarak işlevlendirmeyi 
+amaçlar. Bu akıllı sistemler, büyük veri analizi, 
+görüntü ve ses işleme, dil anlama, otonom araçlar, 
+tıbbi tanı yardımı gibi birçok alanda 
+kullanılabilir."""
